e28971fe3d766e6d7b7ee472309edd114fb3afb7
yabeda79 <43182987+yabeda79@users.noreply.github.com>
2018-09-14 17:00:33 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+let solution=[]
+let d, x1, x2
+equation=equation.split(' ')
+if(equation[7]=='-'){
+  equation[8]=equation[8]*(-1)
+}
+d=Math.pow(equation[4], 2)-(4*equation[0]*equation[8])
+if(equation[3]=='-'){
+  equation[4]=equation[4]*(-1)
+}
+x1=(-equation[4]+Math.sqrt(d))/(2*equation[0])
+x2=(-equation[4]-Math.sqrt(d))/(2*equation[0])
+solution.push(Math.round(x1))
+solution.push(Math.round(x2))
+solution.sort((a, b)=>a-b)
+return solution
 }
